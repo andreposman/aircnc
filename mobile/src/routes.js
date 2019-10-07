1f2f2a092d736455c0605813173dc451ed969ddd
Andre Posman <andre.posman@gmail.com>
2019-10-07 01:11:54 -0300

    Starting the mobile version

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,2 +1,9 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 
+import Login from './pages/Login'
+import Book from './pages/Book'
+import List from './pages/List'
+
+const Routes = createAppContainer(
+    createSwitchNavigator()
+)
